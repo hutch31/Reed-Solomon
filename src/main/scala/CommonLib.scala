850c4d537e9c716059ae8bf28feaf06566c4b4f4
egorman44 <easmirnov44@gmail.com>
2024-03-12 11:28:00 +0400

    [COM_LIB] Add common lib.

---
+++ src/main/scala/CommonLib.scala
@@ -0,0 +1,55 @@
+package Rs
+
+import chisel3._
+import chisel3.util.ImplicitConversions.intToUInt
+import circt.stage.ChiselStage
+import chisel3.util._
+
+class PrioEnc(width: Int) extends Module {
+  val io = IO(new Bundle {
+    val in = Input(UInt(width.W))
+    val out = Output(UInt(log2Ceil(width).W))
+  })
+
+  io.out := 0.U
+  for(i <- (0 until width)) {
+    when(io.in(i) === 1) {
+      io.out := i.U
+    }
+  }
+
+}
+
+class FindFirstSet(width: Int) extends Module {
+  val io = IO(new Bundle {
+    val in = Input(UInt(width.W)) // Assuming a width-bit input vector
+    val out = Output(UInt(width.W)) // Output is a one-hot vector
+  })
+
+  // Find the index of the first set bit
+  val prioEnc = Module(new PrioEnc(width))
+  prioEnc.io.in := io.in
+
+  // Generate a one-hot vector
+  io.out := UIntToOH(prioEnc.io.out, width = width)
+}
+
+class OneHotMux(width: Int) extends Module {
+  require(width > 0, "Width must be greater than 0")
+
+  val io = IO(new Bundle {
+    val in = Input(Vec(width, UInt(8.W))) // Input is a vector of one-hot signals
+                                          //val sel = Input(Vec(width, Bool())) // Selection signal
+    val sel = Input(UInt(width.W)) // Selection signal
+    val out = Output(UInt(8.W)) // Output is the selected one-hot signal
+  })
+
+  io.out := Mux1H(io.sel, io.in)
+}
+
+// runMain Rs.GenFfs
+object GenFfs extends App {
+  ChiselStage.emitSystemVerilogFile(new OneHotMux(4), Array())
+  //ChiselStage.emitSystemVerilogFile(new FindFirstSet(8), Array())
+  //ChiselStage.emitSystemVerilogFile(new ffs(8), Array())
+}
