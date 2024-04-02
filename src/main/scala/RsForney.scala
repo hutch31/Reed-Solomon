c594b244ab92cd46cc92d2651de2857b55eeaf63
egorman44 <easmirnov44@gmail.com>
2024-04-02 23:11:25 +0400

    [FORNEY] Update RsForney.

--- src/main/scala/RsForney.scala
+++ src/main/scala/RsForney.scala
@@ -10,14 +10,19 @@ class RsForney extends Module with GfParams {
     val posArray = Input(new NumPosIf)
     val syndrome = Input(Vec(redundancy, UInt(symbWidth.W)))
     val formalDerIf    = Output(Vec(tLen, UInt(symbWidth.W)))
-    val errorEval = Output(Valid(Vec(tLen, UInt(symbWidth.W))))
+    val errEvalXlInvIf = Output(Valid(new vecFfsIf(tLen)))
   })
+
+  //////////////////////////////////////
+  //  INTERFACES
+  //////////////////////////////////////
 
   //////////////////////////////////////
   // need to convert the positions to coefficients
   // degrees for the errata locator algo to work
   // (eg: instead of [0, 1, 2] it will become [len(msg)-1, len(msg)-2, len(msg) -3])
   //////////////////////////////////////
+
   val ffs = Module(new FindFirstSet(tLen))
   ffs.io.in := io.posArray.sel
 
@@ -28,13 +33,36 @@ class RsForney extends Module with GfParams {
   for(i <- 0 until tLen)
     errPosCoefIf.bits.vec(i) := (nLen-1).U-io.posArray.pos(i)
 
-  
+  //////////////////////////////////////
+  // Xl and XlInv
+  //////////////////////////////////////
+
+  val Xl = Wire(Vec(tLen, (UInt(symbWidth.W))))
+  val XlInv = Wire(Vec(tLen, (UInt(symbWidth.W))))
+
+  for(i <- 0 until tLen) {
+    val coefDiff = (symbNum-1).U-errPosCoefIf.bits.vec(i)
+    Xl(i) := powerFirstRootNeg(coefDiff)
+    XlInv(i) := gfInv(Xl(i))
+  }
+
+  val XlInvFfsIf = Wire(Valid(new vecFfsIf(tLen)))
+  //val XlInvVldIf = Wire(Valid(new vecFfsIf(tLen)))
+
+  XlInvFfsIf.bits.vec := XlInv
+  XlInvFfsIf.bits.ffs := ffs.io.out
+  XlInvFfsIf.valid := io.posArray.valid
+
+  //XlInvVldIf.bits.vec := XlInv
+  //XlInvVldIf.valid := io.posArray.valid
+
   //////////////////////////////////////
   // Modules instantiation
   //////////////////////////////////////
 
   val errataLocator = Module(new ErrataLocatorPar)
-  val errorEvaluator = Module(new ErrorEvaluator)
+  val errorEvaluator = Module(new ErrEval)
+  val errEvalXlInv = Module(new ErrEvalXlInv)
   val formalDer = Module(new FormalDerivative)
 
   // ErrataLocator
@@ -42,16 +70,25 @@ class RsForney extends Module with GfParams {
   errorEvaluator.io.errataLocIf <> errataLocator.io.errataLocIf
   errorEvaluator.io.syndrome := io.syndrome
 
-  formalDer.io.errPosCoefIf <> errPosCoefIf
+  errEvalXlInv.io.errEvalIf <> errorEvaluator.io.errEvalIf
+  errEvalXlInv.io.XlInvIf <> XlInvFfsIf
 
+  formalDer.io.XlInvIf <> XlInvFfsIf
+  formalDer.io.Xl := Xl
+
+  io.errEvalXlInvIf <> errEvalXlInv.io.errEvalXlInvIf
   io.formalDerIf := formalDer.io.formalDerIf
-  io.errorEval := errorEvaluator.io.errorEval
 
+  
 }
+
+// runMain Rs.GenForney
 
 object GenForney extends App {
   //ChiselStage.emitSystemVerilogFile(new ErrataLocatorPar(), Array())
-  //ChiselStage.emitSystemVerilogFile(new ErrorEvaluator(), Array())
+  //ChiselStage.emitSystemVerilogFile(new ErrEval(), Array())
+  //ChiselStage.emitSystemVerilogFile(new ErrEvalXlInv(), Array())
+  //ChiselStage.emitSystemVerilogFile(new ShiftTest(), Array())
   ChiselStage.emitSystemVerilogFile(new RsForney(), Array())
   //ChiselStage.emitSystemVerilogFile(new FormalDerivative(), Array())
   //ChiselStage.emitSystemVerilogFile(new FormalDerivativeStage1(), Array())
