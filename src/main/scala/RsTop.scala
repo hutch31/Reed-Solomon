b49307f8da7eaed2f26b92ea1fa10824ed9857f3
egorman44 <easmirnov44@gmail.com>
2024-04-20 19:32:05 +0400

    [RS] Add RsTop top module.

---
+++ src/main/scala/RsTop.scala
@@ -0,0 +1,25 @@
+package Rs
+
+import chisel3._
+import circt.stage.ChiselStage
+import chisel3.util._
+
+class RsTop extends Module with GfParams {
+  val io = IO(new Bundle {
+    val sAxisIf = Input(Valid(new axisIf(axisWidth)))
+    val errLocIf = Output(Valid(Vec(tLen+1, UInt(symbWidth.W))))
+  })
+
+  val rsSynd = Module(new RsSynd)
+  val rsBm = Module(new RsBm)
+
+  rsSynd.io.sAxisIf <> io.sAxisIf
+  rsBm.io.syndIf <> rsSynd.io.syndIf
+  io.errLocIf <> rsBm.io.errLocIf
+
+}
+
+// runMain Rs.GenRs
+object GenRs extends App {
+  ChiselStage.emitSystemVerilogFile(new Rs(), Array())
+}
