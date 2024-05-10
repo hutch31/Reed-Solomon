34be86dda1ac8e9a51f62526f65ba1fe56d2b9d4
egorman44 <easmirnov44@gmail.com>
2024-05-10 17:08:41 +0400

    [Chien] Use vecFfsIf interface for errLocIf.

--- src/main/scala/RsChien/RsChien.scala
+++ src/main/scala/RsChien/RsChien.scala
@@ -11,16 +11,23 @@ import chisel3.util._
 
 class RsChien extends Module with GfParams{
   val io = IO(new Bundle {
-    val errLocIf = Input(Valid(Vec(tLen+1, UInt(symbWidth.W))))   
+    val errLocIf = Input(Valid(new vecFfsIf(tLen+1)))
     val posArray = Output(new NumPosIf)
+    val chienErrDetect = Output(Bool())
   })
 
   val rsChienErrBitPos = Module(new RsChienErrBitPos)
   val rsChienBitPosToNum = Module(new RsChienBitPosToNum)
 
-  rsChienErrBitPos.io.errLocIf := io.errLocIf
-  rsChienBitPosToNum.io.bitPos := rsChienErrBitPos.io.bitPos
-  io.posArray := rsChienBitPosToNum.io.posArray
+  rsChienErrBitPos.io.errLocIf <> io.errLocIf
+  rsChienBitPosToNum.io.bitPos <> rsChienErrBitPos.io.bitPos
+  io.posArray <> rsChienBitPosToNum.io.posArray
+
+  when(io.posArray.valid) {
+    io.chienErrDetect := (io.errLocIf.bits.ffs(tLen,1) ^ io.posArray.sel).orR
+  }.otherwise {
+    io.chienErrDetect := 0.U
+  }
 }
 
 //
@@ -29,6 +36,6 @@ object GenChien extends App with GfParams{
   //ChiselStage.emitSystemVerilogFile(new GfPolyEvalHorner(tLen, ffStepPolyEval), Array())
   //ChiselStage.emitSystemVerilogFile(new RsChienErrBitPos(), Array())
   //ChiselStage.emitSystemVerilogFile(new RsChienBitPosToNum(), Array())
-  ChiselStage.emitSystemVerilogFile(new FindFirstSetNew(width=chienRootsPerCycle, lsbFirst=true), Array())
-  //ChiselStage.emitSystemVerilogFile(new RsChien(), Array())
+  //ChiselStage.emitSystemVerilogFile(new FindFirstSetNew(width=chienRootsPerCycle, lsbFirst=true), Array())
+  ChiselStage.emitSystemVerilogFile(new RsChien(), Array())
 }
