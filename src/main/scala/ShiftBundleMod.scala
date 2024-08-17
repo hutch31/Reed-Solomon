c4be7dbac1abb0123699559b740ab343e16d63e6
egorman44 <easmirnov44@gmail.com>
2024-08-17 17:09:12 +0500

    [Shift] Add paramameterizable shift period.

--- src/main/scala/ShiftBundleMod.scala
+++ src/main/scala/ShiftBundleMod.scala
@@ -3,7 +3,7 @@ package Rs
 import chisel3._
 import chisel3.util._
 
-class ShiftBundleMod[T <: Data](shiftUnit: T, width: Int, shiftVal: Int) extends Module{
+class ShiftBundleMod[T <: Data](shiftUnit: T, width: Int, shiftVal: Int, shiftCntrLimit: Int=0) extends Module{
   require(shiftVal <= width, "[ERROR] ShiftBundle. Wrong shiftVal compared to the width.")
   val io = IO(new Bundle {
     val vecIn = Input(Valid(Vec(width, shiftUnit)))
@@ -11,21 +11,40 @@ class ShiftBundleMod[T <: Data](shiftUnit: T, width: Int, shiftVal: Int) extends
     val lastOut = Output(Bool())
   })
 
+  val shiftCntrQ = RegInit(UInt(log2Ceil(width).W), 0.U)
+  val shiftEn = Wire(UInt(1.W))
+
+  when(io.vecIn.valid) {
+    shiftCntrQ := 0.U
+  }.otherwise{
+    when(shiftCntrQ === shiftCntrLimit.U) {
+      shiftCntrQ := 0.U
+    }.otherwise {
+      shiftCntrQ := shiftCntrQ + 1.U
+    }
+  }
+
+  if(shiftCntrLimit == 0){
+    shiftEn := 1.U
+  }else{
+    shiftEn := (shiftCntrQ === shiftCntrLimit.U)
+  }
+
   if(shiftVal == width) {
     io.vecOut.bits := io.vecIn.bits
     io.lastOut := io.vecIn.valid
     io.vecOut.valid := io.vecIn.valid
   } else {
 
-    val vldWidth = math.ceil(width/shiftVal.toDouble).toInt
+    val vldWidth  = math.ceil(width/shiftVal.toDouble).toInt
     val dataShift = Reg(Vec(width, shiftUnit))
-    val vldShift = RegInit(UInt(vldWidth.W), 0.U)
+    val vldShift  = RegInit(UInt(vldWidth.W), 0.U)
 
     // Load data into the shift register
     when(io.vecIn.valid) {
       dataShift := io.vecIn.bits
       vldShift := ((1<<vldWidth)-1).U
-    }.otherwise {
+    }.elsewhen(shiftEn === 1.U) {
       vldShift := vldShift >> 1
       for(i <- 0 until width-shiftVal)
         dataShift(i) := dataShift(i+shiftVal)
@@ -37,7 +56,7 @@ class ShiftBundleMod[T <: Data](shiftUnit: T, width: Int, shiftVal: Int) extends
       io.lastOut := 1.U
     }.otherwise {
       io.lastOut := 0.U
-    }    
+    }
 
     for(i <- 0 until shiftVal){
       io.vecOut.bits(i) := dataShift(i)
