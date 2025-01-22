73bd19d0f048d42f99ebf9c4143c5c989818d57d
egorman44 <easmirnov44@gmail.com>
2025-01-22 14:16:23 +0400

    [Chien] Update CapturePositions block.

--- src/main/scala/CapturePositions.scala
+++ src/main/scala/CapturePositions.scala
@@ -10,19 +10,26 @@ class CapturePositions(c:Config) extends Module {
     val errPosIf = Output(Valid(new vecFfsIf(c.T_LEN, c.SYMB_WIDTH)))    
   })
 
+  class PositionsVld extends Bundle {
+    val positions = UInt(c.SYMB_WIDTH.W)
+    val vld = Bool()
+  }
+
+  val zeroUnit = 0.U.asTypeOf(new PositionsVld)
+
   ////////////////////////////////
   // Extract position from incomming 
   // bit vector where bits are asserted
   ////////////////////////////////
 
   val extrPos = Module(new ExtractPositions(c.chienRootsPerCycle, c.T_LEN, 4))
-  val numStages = extrPos.numStages
+  val numStages = extrPos.numStages + 1
 
   extrPos.io.in.bits := io.bitPos.pos
   extrPos.io.in.valid := io.bitPos.valid
 
   // Shift last
-  val lastQ = Reg(Vec(numStages, Bool()))
+  val lastQ = RegInit(VecInit(Seq.fill(numStages)(false.B)))
 
   lastQ(0) := io.bitPos.last
   for(i <- 1 until numStages) {
@@ -33,58 +40,52 @@ class CapturePositions(c:Config) extends Module {
   // insertVec is used to capture and shift
   // valid positions into shift register of
   // length T_LEN.
-  //
-  // outShift is used to remove bubbles in
-  // the shift registers since number of
-  // errors could be less than T_LEN.
   ////////////////////////////////
 
+  // Output width is T_LEN+1 to capture the error condition
   val insertVec = Module(new InsertVec(new PositionsVld,c.T_LEN, c.T_LEN))
-  val baseVec = Reg(Vec(c.T_LEN, new PositionsVld))
+
   val vecIn   = Wire(Vec(c.T_LEN, new PositionsVld))
   val baseCntr = RegInit(UInt(log2Ceil(c.SYMB_NUM).W), 0.U)
+  //val baseVec = Reg(Vec(c.T_LEN, new PositionsVld))
+  val baseVec = RegInit(VecInit(Seq.fill(c.T_LEN)(zeroUnit)))
+  val outReg  = RegInit(VecInit(Seq.fill(c.T_LEN)(zeroUnit)))
 
-  // Final shift
-  val outShift = Module(new BarrelShifter(new PositionsVld, c.T_LEN))
-  val outputVec = Reg(Vec(c.T_LEN, new PositionsVld))
-
-  class PositionsVld extends Bundle {
-    val positions = UInt(c.SYMB_WIDTH.W)
-    val vld = Bool()
-  }
-
+  //The shift value is defined by the valid numbers in the vector.
+  insertVec.io.shiftVal := baseVec.map(_.vld.asUInt).reduce(_ +& _)
   insertVec.io.vecIn := vecIn
   insertVec.io.baseVec := baseVec
-  insertVec.io.shiftVal := extrPos.io.outTkeep.map(_.asUInt).reduce(_ +& _)
-
-  outShift.io.vecIn := insertVec.io.vecOut
-  outShift.io.shiftVal := insertVec.io.vecOut.map(bundle => (!bundle.vld).asUInt).reduce(_ +& _)
-
+  
   // need to add baseCntr to get a proper position:
   when(extrPos.io.outValid) {
     when(lastQ(numStages-1)) {
       baseCntr := 0.U
       baseVec.foreach(x => x := 0.U.asTypeOf(new PositionsVld))
-      outputVec := outShift.io.vecOut
-      //outputVec := outShift.io.vecOut
     }.otherwise{
       baseCntr := baseCntr + c.chienRootsPerCycle.U
-      baseVec := insertVec.io.vecOut
+      // Capture only valid output
+      for(i <- 0 until c.T_LEN) {
+        baseVec(i).vld := insertVec.io.vecOut(i).vld
+        when(insertVec.io.vecOut(i).vld) {
+          baseVec(i).positions := insertVec.io.vecOut(i).positions
+        }
+      }
     }
   }
 
   for(i <- 0 until c.T_LEN) {
     vecIn(i).positions := extrPos.io.positions(i) + baseCntr
     vecIn(i).vld := extrPos.io.outTkeep(i)
-    io.errPosIf.bits.vec(i) := outputVec(i).positions
   }
 
-  //io.errPosIf.bits.ffs := Reverse((VecInit(outputVec.map(_.vld))).asUInt)
-  //io.errPosIf.bits.vec := insertVec.io.vecOut
-  
-  io.errPosIf.valid := lastQ(numStages-1)
-  io.errPosIf.bits.ffs := (VecInit(insertVec.io.vecOut.map(_.vld))).asUInt
-  io.errPosIf.bits.vec := (VecInit(insertVec.io.vecOut.map(_.positions)))
+  when(lastQ(numStages-1)){
+    outReg := insertVec.io.vecOut
+  }
+
+  io.errPosIf.bits.ffs := (VecInit(outReg.map(_.vld))).asUInt
+  io.errPosIf.bits.vec := (VecInit(outReg.map(x => Mux(x.vld, c.N_LEN.U - 1.U - x.positions, 0.U))))
+  io.errPosIf.valid := RegNext(lastQ(numStages-1), init = false.B)
+
 }
 
 object GenCapturePositions extends App {
