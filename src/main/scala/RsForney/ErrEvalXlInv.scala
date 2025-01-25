28703e403d6211e7d2f20f60b2fe44a88985a0cd
egorman44 <easmirnov44@gmail.com>
2025-01-25 23:28:40 +0400

    [RsForney] Add notReadyAssrt assertion for ErrEvalXlInv. Register sel signal.

--- src/main/scala/RsForney/ErrEvalXlInv.scala
+++ src/main/scala/RsForney/ErrEvalXlInv.scala
@@ -19,53 +19,64 @@ class ErrEvalXlInv(c: Config) extends Module {
   shiftVec.io.vecIn.valid := io.errEvalIf.valid
 
   for(i <- 0 until c.forneyEEXlInvTermsPerCycle) {
-    stage(i).io.vecIn.bits := io.errEvalIf.bits.vec
-    stage(i).io.vecIn.valid := shiftVec.io.lastOut
+    stage(i).io.errEvalIf := io.errEvalIf.bits
+    // Send last down to stage to detect
+    // when the accumulator is full
+    stage(i).io.eopIn := shiftVec.io.lastOut
     stage(i).io.XlInvSymb := shiftVec.io.vecOut.bits(i)
-    stageOut(i) := stage(i).io.vecOut.bits
+    stageOut(i) := stage(i).io.vecOut
   }
 
   ///////////////////////////////////
   // Accum matrix
   ///////////////////////////////////
 
+  val sel = Reg(UInt((c.T_LEN+1).W))
+  
   // The matrix fully loaded into accumMat when lastQ is asserted
-  val lastQ = RegNext(stage(0).io.vecOut.valid)
+  when(stage(0).io.eopOut) {
+    sel := io.errEvalIf.bits.ffs << 1.U
+  }
+  dontTouch(sel)
+
+  val lastQ = RegNext(stage(0).io.eopOut)
 
   val accumMat = Module(new AccumMat(c.SYMB_WIDTH, c.T_LEN+1, c.forneyEEXlInvTermsPerCycle, c.forneyEEXlInvShiftLatency, c.T_LEN))
 
   accumMat.io.vecIn := stageOut
-
-  //val sel = io.XlInvIf.bits.ffs << 1
-  val sel = io.errEvalIf.bits.ffs << 1
+  
   val errEvalXlInvVec = Reg(Vec(c.T_LEN, UInt(c.SYMB_WIDTH.W)))
   val errEvalXlInvVld = RegNext(next=lastQ, 0.U)
-  val errEvalXlInvFfs = Reg(UInt(c.T_LEN.W))
 
   when(lastQ) {
     errEvalXlInvVec := Mux1H(sel, accumMat.io.matTOut)
-    errEvalXlInvFfs := io.errEvalIf.bits.ffs
   }
-
-  val errEvalFfs = io.errEvalIf.bits.ffs
-  dontTouch(errEvalFfs)
-
+  
   ///////////////////////////////////
   // Output signal
   ///////////////////////////////////
 
   io.errEvalXlInvIf.bits.vec := errEvalXlInvVec
   io.errEvalXlInvIf.valid := errEvalXlInvVld
-  // TODO : What FFS to connect ?! 
-  io.errEvalXlInvIf.bits.ffs := errEvalXlInvFfs
+  // Don't need ffs, since it's not used in ErrVal block
+  io.errEvalXlInvIf.bits.ffs := 0.U
 
+  /////////////////
+  // Assert not ready
+  /////////////////
+  val notReadyAssrt = Module(new NotReadyAssrt())
+  notReadyAssrt.io.start := io.errEvalIf.valid
+  notReadyAssrt.io.stop := stage(0).io.eopOut
+  
 }
 
 class ErrEvalXlInvStage(c: Config) extends Module {
   val io = IO(new Bundle {
-    val vecIn = Input(Valid(Vec(c.T_LEN+1, UInt(c.SYMB_WIDTH.W))))
+    val errEvalIf = Input(new vecFfsIf(c.T_LEN+1, c.SYMB_WIDTH))
     val XlInvSymb = Input(UInt(c.SYMB_WIDTH.W))
-    val vecOut = Output(Valid(Vec(c.T_LEN+1, UInt(c.SYMB_WIDTH.W))))
+    val eopIn = Input(Bool())
+    val eopOut = Output(Bool())
+    val vecOut = Output(Vec(c.T_LEN+1, UInt(c.SYMB_WIDTH.W)))
   })
 
   val qStage = Reg(Vec(c.forneyEEXlInvQStages, (Vec(c.T_LEN+1, UInt(c.SYMB_WIDTH.W)))))
@@ -73,8 +84,9 @@ class ErrEvalXlInvStage(c: Config) extends Module {
 
   val qXlInvStage = Reg(Vec(c.forneyEEXlInvQStages, UInt(c.SYMB_WIDTH.W)))
 
-  io.vecOut.bits := qStage(c.forneyEEXlInvQStages-1)
-  io.vecOut.valid := ShiftRegister(io.vecIn.valid, c.forneyEEXlInvQStages, false.B, true.B)
+  io.vecOut := qStage(c.forneyEEXlInvQStages-1)
+
+  io.eopOut := ShiftRegister(io.eopIn, c.forneyEEXlInvQStages, false.B, true.B)
 
   for(i <- 0 until c.forneyEEXlInvQStages) {
     val start_indx = 1+i*c.forneyEEXlInvComboLen
@@ -83,7 +95,7 @@ class ErrEvalXlInvStage(c: Config) extends Module {
     val initXlInv = Wire(UInt(c.SYMB_WIDTH.W))
 
     if(i == 0) {
-      initStage := io.vecIn.bits
+      initStage := io.errEvalIf.vec
       initXlInv := io.XlInvSymb
     }
     else {
