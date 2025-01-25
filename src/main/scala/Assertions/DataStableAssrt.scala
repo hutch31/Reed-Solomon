4c706214c96eeb9727dc67ffa039860e896ba8ae
egorman44 <easmirnov44@gmail.com>
2025-01-25 20:47:19 +0400

    [ASSRT] Add NotReadyAssrt assertion.

--- src/main/scala/Assertions/DataStableAssrt.scala
+++ src/main/scala/Assertions/DataStableAssrt.scala
@@ -18,6 +18,26 @@ class DataStableAssrt[T <: Data](gen: T) extends Module {
   }
 
   val stableError = checkEn & ~io.start & prevData.asUInt =/= io.data.asUInt
+  dontTouch(stableError)
   assert(!stableError, "[ERROR] Data is not stable")
 
+}
+
+class NotReadyAssrt extends Module {
+  val io = IO(new Bundle {
+    val start = Input(Bool())
+    val stop = Input(Bool())
+  })
+
+  val busy = RegInit(Bool(), false.B)
+
+  when(io.start) {
+    busy := true.B
+  }.elsewhen(io.stop) {
+    busy := false.B
+  }
+
+  val notReady = busy & io.start
+  dontTouch(notReady)
+  assert(!notReady, "[ERROR] Logic is not ready to handle next data")
 }
